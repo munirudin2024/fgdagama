61c04917e598932853f697d4a65eafbfe9d598e6
munirudin2024 <2024.muhammadmunirudin@student.unsia.ac.id>
2025-11-15 22:50:54 +0000

    feat: Add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = 'fgdagama'; 
+
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: `/${fgdagama}/`,
+  base: `/${repoName}/`, 
   plugins: [react()],
 })
